d062a13d06ddc94f9a79a383224d44ad12ff4469
D.gabriel <you@example.com>
2025-11-11 17:27:23 -0300

    add directory public

---
+++ public/script.js
@@ -0,0 +1,29 @@
+document.getElementById("buscar").addEventListener("click", async ()=> {
+    const cidade = document.getElementById("cidade").value.trim();
+    const resultado = document.getElementById("resultado");
+
+    console.log(cidade);
+    
+    if(!cidade){
+        resultado.innerHTML = `<p class="text-red-500">Digite uma cidade!</p>`;
+        return; //não entendi esse return 
+    };
+     
+    try{
+        const url =  await fetch(`http://localhost:3000/api/clima/${cidade}`);
+        if(!url) throw new Error("erro ao buscar");
+        const dados = await url.json();    
+        const {name, sys, main} = dados;
+        console.log(main.temp)
+        resultado.innerHTML = `
+          <div class="bg-blue-400  rounded-lg p-4 mt-3">
+            <h2 class="text-lg  font-bold">${name}, ${sys.country}</h2>
+            <p class="text-4xl font-bold text-blue-600">${main.temp}ºC</p>
+          </div>
+        `     
+    }   
+    catch(erro){
+        resultado.innerHTML = `<p class="text-red-500">Cidade não encontrada!</p>`;
+
+    }
+});
